5b018fadf0cd7bf5245fd1aa7c9dc912cca8eb12
biyuhuaping <biyuhuaping@163.com>
2021-01-27 16:38:45 +0800

    全选按钮的交互效果

--- lib/pages/cart_page/cart_bottom.dart
+++ lib/pages/cart_page/cart_bottom.dart
@@ -1,31 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provide/provide.dart';
+import '../../provide/cart.dart';
 
 class CartBottom extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(5.0),
-      color: Colors.white,
-      child: Row(
-        children: <Widget>[
-          selectAllBtn(),
-          allPriceArea(),
-          goButton()
-        ],
-      ),
+        margin: EdgeInsets.all(5.0),
+        color: Colors.white,
+        width: ScreenUtil().setWidth(750),
+        child: Provide<CartProvide>(
+          builder: (context,child,childCategory){
+            return  Row(
+              children: <Widget>[
+                selectAllBtn(context),
+                allPriceArea(context),
+                goButton(context)
+              ],
+            );
+          },
+        )
     );
   }
 
   //全选按钮
-  Widget selectAllBtn(){
+  Widget selectAllBtn(context){
+    bool isAllcheck = Provide.value<CartProvide>(context).isAllCheck;
     return Container(
       child: Row(
         children: <Widget>[
           Checkbox(
-            value: true,
+            value: isAllcheck,
             activeColor: Colors.pink,
-            onChanged: (bool val){},
+            onChanged: (bool val){
+              Provide.value<CartProvide>(context).changeAllCheckBtnState(val);
+            },
           ),
           Text('全选')
         ],
@@ -34,7 +44,8 @@ class CartBottom extends StatelessWidget {
   }
 
   // 合计区域
-  Widget allPriceArea(){
+  Widget allPriceArea(context){
+    double allPrice = Provide.value<CartProvide>(context).allPrice;
     return Container(
       width: ScreenUtil().setWidth(430),
       alignment: Alignment.centerRight,
@@ -56,7 +67,7 @@ class CartBottom extends StatelessWidget {
                 alignment: Alignment.centerLeft,
                 width: ScreenUtil().setWidth(150),
                 child: Text(
-                  '¥198812',
+                  '¥${allPrice}',
                   style: TextStyle(
                     fontSize: ScreenUtil().setSp(36),
                     color: Colors.red
@@ -82,7 +93,8 @@ class CartBottom extends StatelessWidget {
   }
 
   //结算按钮
-  Widget goButton(){
+  Widget goButton(context){
+    int allGoodsCount = Provide.value<CartProvide>(context).allGoodsCount;
     return Container(
       width: ScreenUtil().setWidth(160),
       padding: EdgeInsets.only(left: 10),
@@ -96,7 +108,7 @@ class CartBottom extends StatelessWidget {
             borderRadius: BorderRadius.circular(3.0)
           ),
           child: Text(
-            '结算(6)',
+            '结算(${allGoodsCount})',
             style: TextStyle(
                 color: Colors.white
             ),
--- lib/provide/cart.dart
+++ lib/provide/cart.dart
@@ -6,6 +6,9 @@ import 'dart:convert';
 class CartProvide with ChangeNotifier{
   String cartString = "[]";
   List<CartInfoMode> cartList = []; //商品列表对象
+  double allPrice = 0;//总价格
+  int allGoodsCount = 0;//商品总数量
+  bool isAllCheck = true; //是否全选
 
 
   save(goodsId,goodsName,count,price,images) async{
@@ -20,11 +23,19 @@ class CartProvide with ChangeNotifier{
     bool isHave = false;//默认为没有
     int ival = 0;//用于进行循环的索引使用
 
+    allPrice = 0;
+    allGoodsCount = 0;  //把商品总数量设置为0
+
     tempList.forEach((item) {//进行循环，找出是否已经存在该商品
+      //如果存在，数量进行+1操作
       if(item['goodsId'] == goodsId){
         tempList[ival]['count'] = item['count']+1;
         cartList[ival].count ++;
         isHave = true;
+      }
+      if(item['isCheck']){
+        allPrice += (cartList[ival].price * cartList[ival].count);
+        allGoodsCount += cartList[ival].count;
       }
       ival++;
     });
@@ -41,6 +52,8 @@ class CartProvide with ChangeNotifier{
       };
       tempList.add(newGoods);
       cartList.add(new CartInfoMode.fromJson(newGoods));
+      allPrice += (count * price);
+      allGoodsCount += count;
     }
 
     //把字符串进行encode操作，
@@ -72,7 +85,16 @@ class CartProvide with ChangeNotifier{
         cartList = [];
       }else{
         List<Map> tempList = (json.decode(cartString.toString()) as List).cast();
+        allPrice = 0;
+        allGoodsCount = 0;
+        isAllCheck = true;
         tempList.forEach((item) {
+          if(item['isCheck']){
+            allPrice += (item['count'] * item['price']);
+            allGoodsCount += item['count'];
+          }else{
+            isAllCheck = false;
+          }
           cartList.add(new CartInfoMode.fromJson(item));
         });
       }
@@ -100,6 +122,41 @@ class CartProvide with ChangeNotifier{
     cartString = json.encode(tempList).toString();
     prefs.setString('cartInfo', cartString);//进行持久化
     getCartInfo();
+  }
+
+  //修改选中状态
+  changeCheckState(CartInfoMode cartItem) async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    cartString = prefs.getString('cartInfo');
+    List<Map> tempList = (json.decode(cartString.toString()) as List).cast();
+    int tempIndex = 0;
+    int changeIndex = 0;
+    tempList.forEach((item) {
+      if(item['goodsId'] == cartItem.goodsId){
+        changeIndex = tempIndex;
+      }
+      tempIndex ++;
+    });
+    tempList[changeIndex] = cartItem.toJson();
+    cartString = json.encode(tempList).toString();
+    prefs.setString('cartInfo', cartString);
+    await getCartInfo();
+  }
+
+  //点击全选按钮操作
+  changeAllCheckBtnState(bool isCheck) async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    cartString = prefs.getString('cartInfo');
+    List<Map> tempList = (json.decode(cartString.toString()) as List).cast();
+    List<Map> newList = [];
+    for(var item in tempList){
+      var newItem = item;
+      newItem['isCheck'] = isCheck;
+      newList.add(newItem);
+    }
+    cartString = json.encode(newList).toString();
+    prefs.setString('cartInfo', cartString);
+    await getCartInfo();
   }
 
 }
